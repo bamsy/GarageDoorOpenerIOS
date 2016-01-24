03afdaddc5f91393b4e87e2a2cda32bf770a8d71
bamsy <bamsy23@gmail.com>
2016-01-24 14:22:14 -0600

    initial commit

--- GarageDoorOpener/ViewController.swift
+++ GarageDoorOpener/ViewController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldWS: UITextField!
+    @IBAction func buttonPressed(sender: AnyObject) {
+        if(textFieldWS.text!.characters.count == 0){
+            print("empty string")
+        } else {
+            print(textFieldWS.text!)
+            //let postEndPoint: String = textFieldWS.text!
+            let postEndPoint: String = "http://jsonplaceholder.typicode.com/posts/1"
+            guard let url = NSURL(string:postEndPoint) else {
+                print("Error: cannot create URL")
+                return
+            }
+            let urlRequest = NSURLRequest(URL: url)
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            let task = session.dataTaskWithRequest(urlRequest, completionHandler: { (data,response,error) in
+                guard let responseData = data else {
+                    print("Did not receive data")
+                    return
+                }
+                
+                guard error == nil else {
+                    print("error calling GET "+self.textFieldWS.text!)
+                    print(error)
+                    return
+                }
+                
+                print(responseData.length)
+            })
+            task.resume()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello World")
     }
 
     override func didReceiveMemoryWarning() {
